acde85b4717a0c59f78a0ee22538f05ef56c9a80
xiaopan <1193468221@qq.com>
2024-01-15 03:38:03 +0800

    version2.1 提交了 hot_fix分支的代码

--- Test1/src/Tesss.java
+++ Test1/src/Tesss.java
@@ -1,6 +1,6 @@
 public class Tesss {
     public static void main(String[] args) {
         System.out.println("a");
-        System.out.println("b");
+        System.out.println("master");
     }
 }
