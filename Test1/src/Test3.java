5aa330f801e0de2d11ec435506d82f381938a45d
潘 <155409150+XiaoPanMi@users.noreply.github.com>
2024-01-15 05:11:25 +0800

    Update Test3.java
    演示拉取

--- Test1/src/Test3.java
+++ Test1/src/Test3.java
@@ -3,6 +3,8 @@ import java.sql.SQLOutput;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("test3");
+          System.out.println("testpull拉取");
+        
     }
 }
 
